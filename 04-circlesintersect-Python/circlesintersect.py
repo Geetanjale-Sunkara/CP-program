e18e839cbe093b764d1fc4197d7ea4289fee2f4e
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-14 01:26:55 +0530

    Jul 14  01:26:55

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -7,7 +7,7 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
     # your code goes here
-    if (((x1-x2)**2+(y1-y2)**2) > ((r1+r2)**2)):
+    if (((x1-x2)**2+(y1-y2)**2) == ((r1+r2)**2)):
         return True
     else:
         return False
