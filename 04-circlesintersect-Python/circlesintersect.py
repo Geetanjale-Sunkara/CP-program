a55d7a90fbe5758dbdec4875fce13058af58936f
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-14 01:30:50 +0530

    Jul 14  01:30:50

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -1,3 +1,4 @@
+import math
 # Write the function circlesIntersect(x1, y1, r1, x2, y2, r2)
 # that takes 6 numbers (ints) -- x1, y1, r1, x2, y2, r2 --
 # that describe the circle centered at (x1,y1) with radius r1,
@@ -7,7 +8,7 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
     # your code goes here
-    if (((x1-x2)**2+(y1-y2)**2) == ((r1+r2)**2)):
+    if (math.sqrt((x1-x2)**2+(y1-y2)**2) == ((r1+r2)**2)):
         return True
     else:
         return False
