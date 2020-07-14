3bbe90cc85173f2f3da335c2e9d78c219edc38f2
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-14 19:31:38 +0530

    Jul 14  19:31:38

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -5,8 +5,8 @@ iterative code in the instructions."""
 
 
 def get_fib(position):
+    # Pytest check
     if (position <= 1):
         return position
     else:
         return (get_fib(position-1)+get_fib(position-2))
-    pass
