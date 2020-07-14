a1d2ea83785967fa2912da1baa6ae9a745fc8baa
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-14 17:59:10 +0530

    a

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -6,15 +6,17 @@ Think about this while you're implementing:
 why is it easier to add an "insert_first"
 function than just use "append"?"""
 
+
 class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-        
+
     def append(self, new_element):
         current = self.head
         if self.head:
@@ -26,21 +28,30 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
+        new_element.next = self.head
+        self.head = new_element
         pass
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
+        if self.head == None:
+            return None
+        ele = self.head
+        self.head = self.head.next
+        return ele
         pass
 
+
 class stack(object):
-    def __init__(self,top=None):
+    def __init__(self, top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
         pass
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
+        return self.ll.delete_first()
         pass
-    
