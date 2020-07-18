7c5a7db992e5e27e4b3e2cefe2e04d3b094f5a29
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-19 00:33:55 +0530

    a

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -3,11 +3,18 @@
 // # so "A" and "a" are both vowels. The vowels are "a", "e", "i", "o", and "u". 
 // # So, for example, ("Abc def!!! a? yzyzyz!") returns 3 (two a's and one e).
 
-
 class vowelscount {
-	public int fun_vowelscount(String s){
+	public int fun_vowelscount(String s) {
 		// your code goes here
-		return 0;
+		s = s.toLowerCase();
+		int cnt = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				cnt += 1;
+			}
+		}
+		return cnt;
 	}
-	
+
 }
