910317ce9b378a38009d1cde6194377c42a8b0c8
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-19 13:18:59 +0530

    Jul 19  13:18:59

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -1,8 +1,17 @@
 # median(a) (10 pts)
-# Write the non-destructive function median(a) that takes a list of ints or floats and returns the median value, 
-# which is the value of the middle element, or the average of the two middle elements if there is no single middle 
+# Write the non-destructive function median(a) that takes a list of ints or floats and returns the median value,
+# which is the value of the middle element, or the average of the two middle elements if there is no single middle
 # element. If the list is empty, return None.
 
+
 def median(a):
-	# your code goes here
-	pass
+    # your code goes here
+    l = len(a)
+    if (l == 0):
+        return None
+    elif (l == 1):
+        return a[0]
+    elif (l % 2 != 0):
+        return a[((l-1)/2)]
+    else:
+        return ((a[int((l/2)-1)]+a[int(l/2)])/2)
