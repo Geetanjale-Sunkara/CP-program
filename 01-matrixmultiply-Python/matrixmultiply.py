113a1a2467c8110ea32f0723c0db767342e6012f
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-21 17:22:14 +0530

    Jul 21  17:22:14

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -1,12 +1,21 @@
-# Write the function matrixMultiply(m1, m2) that takes two 2d lists 
-# (that we will consider to be matrices) and returns a new 2d list that 
-# is the result of multiplying the two matrices. Return None if the 
+# Write the function matrixMultiply(m1, m2) that takes two 2d lists
+# (that we will consider to be matrices) and returns a new 2d list that
+# is the result of multiplying the two matrices. Return None if the
 # two matrices cannot be multiplied for any reason.
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
-
-
-
-
+    if len(m1[0]) == len(m2):
+        result = [[0 for i in range(len(m1))] for j in range(len(m2[0]))]
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                result += m1[i][j]*m2[j][i]
+        return result
+    elif len(m1) == len(m2[0]):
+        result = [[0 for i in range(len(m2))] for j in range(len(m1[0]))]
+        for i in range(len(m2)):
+            for j in range(len(m1[0])):
+                result += m1[i][j]*m2[j][i]
+        return result
+    else:
+        return None
