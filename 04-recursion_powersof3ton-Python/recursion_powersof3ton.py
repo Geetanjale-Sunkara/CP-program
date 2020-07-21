478c3a60baccfa71fde46d0da38e00df774fe8a8
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-21 20:11:11 +0530

    Jul 21  20:11:11

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -6,7 +6,7 @@ import math
 
 
 def recursion_powersof3ton(n):
-    if math.floor(n) == 1:
+    if math.floor(n) == 1 or math.floor(n) == 2:
         return [1]
     elif math.floor(n) == 0:
         return None
