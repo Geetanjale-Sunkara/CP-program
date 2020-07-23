30efcfce2db9396226b8c84cc0fe6366c3c63d50
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-23 11:17:26 +0530

    a

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -8,6 +8,11 @@
 def fun_carrylessadd(x, y):
     l1 = list(map(int, list(str(x))))
     l2 = list(map(int, list(str(y))))
+    for i in range(abs(len(l1)-len(l2))):
+        if (len(l1) < len(l2)):
+            l1.insert(0, 0)
+        else:
+            l2.insert(0, 0)
     out = [str((l1[i]+l2[i])-10) if l1[i]+l2[i] >=
            10 else str(l1[i]+l2[i]) for i in range(len(l1))]
     return int("".join(out))
