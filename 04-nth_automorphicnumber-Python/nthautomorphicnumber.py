7af3484a3c1c0ffb1291412b7a648ce12b967e3e
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-24 23:09:27 +0530

    Jul 24  23:09:27

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -11,10 +11,43 @@ def isautomorphic(n):
 
 
 def nthautomorphicnumbers(n):
-    i = 0
-    cnt = 1
+    if (n == 1):
+        return 0
+    elif (n == 2):
+        return 1
+    l = [0, 1, 5, 6]
+    num1 = 5
+    l1 = 1
+    num2 = 6
+    l2 = 1
+    cnt = 4
     while (cnt < n):
-        i += 1
-        if (isaotomorphic(i)):
-            cnt += 1
-        return i
+        for i in range(1, 10):
+            nn = i*(10**(l1))+num1
+            if (isautomorphic(nn)):
+                num1 = nn
+                cnt += 1
+                break
+        for i in range(1, 10):
+            nn = i*(10**(l2))+num2
+            if (isautomorphic(nn)):
+                num2 = nn
+                cnt += 1
+                break
+        if (num1 > num2):
+            if num2 not in l:
+                l.append(num2)
+            if num1 not in l:
+                l.append(num1)
+        else:
+            if num1 not in l:
+                l.append(num1)
+            if num2 not in l:
+                l.append(num2)
+        l1 += 1
+        l2 += 1
+    print(l)
+    if len(l) == n:
+        return l[len(l)-1]
+    else:
+        return l[len(l)-2]
