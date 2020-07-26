4a6d94cb0e6f3736ab44688ca95eb28a3c8fdb5d
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-26 19:43:47 +0530

    Jul 26  19:43:47

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -6,10 +6,24 @@
 # If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
-from difflib import SequenceMatcher
 
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    sm = SequenceMatcher(None, s1, s2)
-    return sm.find_longest_match(0, len(s1), len(s2))
+    m = len(s1)
+    n = len(s2)
+    c = [[0 for j in range(n+1)] for i in range(m+1)]
+    l = 0
+    lcs = []
+    for i in range(m):
+        for j in range(n):
+            if s1[i] == s2[j]:
+                cc = c[i][j]+1
+                c[i+1][j+1] = cc
+                if cc > l:
+                    lcs = []
+                    l = cc
+                    lcs.append(s1[i-c+1:i+1])
+                elif cc == l:
+                    lcs.append(s1[i-c+1:i+1])
+    return lcs[len(lcs)-1]
