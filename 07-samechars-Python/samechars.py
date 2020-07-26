7c86ca0e5bb8f36bacbb80728501ee1627f8f07e
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-26 20:29:52 +0530

    a

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -1,10 +1,15 @@
 # sameChars(s1, s2) [20 pts]
-# Write the function sameChars(s1, s2) that takes two strings and returns True if the two strings are composed of 
-# the same characters (though perhaps in different numbers and in different orders) -- that is, if every character 
-# that is in the first string, is in the second, and vice versa -- and False otherwise. This test is 
-# case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
+# Write the function sameChars(s1, s2) that takes two strings and returns True if the two strings are composed of
+# the same characters (though perhaps in different numbers and in different orders) -- that is, if every character
+# that is in the first string, is in the second, and vice versa -- and False otherwise. This test is
+# case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either
 # parameter is not a string, but returns True if both strings are empty (why?).
 
+
 def samechars(s1, s2):
-	# Your code goes here
-	pass
+    ss1 = list(dict.fromkeys(list(str(s1))))
+    ss2 = list(dict.fromkeys(list(str(s2))))
+    if (sorted(ss1) == sorted(ss2)):
+        return True
+    else:
+        return False
