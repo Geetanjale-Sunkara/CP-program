fd89793d74d241150c30b6c4487e9db36a75bd64
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-07-27 12:28:12 +0530

    a

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -3,21 +3,24 @@ class Node(object):
         self.value = value
         self.edges = []
 
+
 class Edge(object):
     def __init__(self, value, node_from, node_to):
         self.value = value
         self.node_from = node_from
         self.node_to = node_to
 
+
 class Graph(object):
     def __init__(self, nodes=[], edges=[]):
         self.nodes = nodes
         self.edges = edges
+        self.max_index = 0
 
     def insert_node(self, new_node_val):
         new_node = Node(new_node_val)
         self.nodes.append(new_node)
-        
+
     def insert_edge(self, new_edge_val, node_from_val, node_to_val):
         from_found = None
         to_found = None
@@ -29,9 +32,11 @@ class Graph(object):
         if from_found == None:
             from_found = Node(node_from_val)
             self.nodes.append(from_found)
+            self.max_index = max(self.max_index, node_from_val)
         if to_found == None:
             to_found = Node(node_to_val)
             self.nodes.append(to_found)
+            self.max_index = max(self.max_index, node_to_val)
         new_edge = Edge(new_edge_val, from_found, to_found)
         from_found.edges.append(new_edge)
         to_found.edges.append(new_edge)
@@ -39,13 +44,24 @@ class Graph(object):
 
     def get_edge_list(self):
         edge_list = []
+        for i in self.edges:
+            edge_list.append((i.value, i.node_from.value, i.node_to.value))
         return edge_list
 
     def get_adjacency_list(self):
-        adjacency_list = [None] * (max_index + 1)
+        adjacency_list = [None] * (self.max_index + 1)
+        for i in self.edges:
+            if adjacency_list[i.node_from.value] == None:
+                adjacency_list[i.node_from.value] = [
+                    (i.node_to.value, i.value)]
+            else:
+                adjacency_list[i.node_from.value].append(
+                    (i.node_to.value, i.value))
         return adjacency_list
-    
-    
+
     def get_adjacency_matrix(self):
-        adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
+        adjacency_matrix = [
+            [0 for i in range(self.max_index + 1)] for j in range(self.max_index + 1)]
+        for i in self.edges:
+            adjacency_matrix[i.node_from.value][i.node_to.value] = i.value
         return adjacency_matrix
